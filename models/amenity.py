92fbd7a8b58d8bdc623748cd03dedef783aafaeb
MainaKamau92 <lewiikamaa8@gmail.com>
2023-03-19 13:22:14 +0300

    Update documentation
    Signed-off-by: MainaKamau92 <lewiikamaa8@gmail.com>

--- models/amenity.py
+++ models/amenity.py
@@ -4,4 +4,9 @@ from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
+    """
+    Amenity class that inherits from BaseModel
+    Attributes:
+        name: input name
+    """
     name = ""
