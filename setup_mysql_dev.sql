d7bd6b93724136b6507fd77081c51f296da6d125
MainaKamau92 <lewiikamaa8@gmail.com>
2023-03-22 18:44:13 +0300

    setup script for test db
    Signed-off-by: MainaKamau92 <lewiikamaa8@gmail.com>

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,4 @@
--- Creates a MySQL server with requisite users and databases for the AirBnB clone project
+-- Creates a MySQL dev server with requisite users and databases for the AirBnB clone project
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
