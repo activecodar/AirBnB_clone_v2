06482d35de02d4017948e40f246d1d77e450a3ae
MainaKamau92 <lewiikamaa8@gmail.com>
2023-03-23 16:45:23 +0300

    Update User model to use sqlalchemy
    Signed-off-by: MainaKamau92 <lewiikamaa8@gmail.com>

--- models/user.py
+++ models/user.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
+"""
+This module defines a class User model for the database
+"""
+from sqlalchemy import Column, String
+
+from models.base_model import BaseModel, Base
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    """
+    User class inherits from BaseModel and Base and defines
+    attributes for a user object in the database
+    Attributes:
+        email (str): email of the user
+        password (str): password of the user
+        first_name (str): first name of the user
+        last_name (str): last name of the user
+    """
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
