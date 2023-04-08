c4ef3e09d8edb1a1a02120898bc8ba3fd57bc7f1
MainaKamau92 <lewiikamaa8@gmail.com>
2023-04-08 13:34:56 +0300

    deploy script update
    Signed-off-by: MainaKamau92 <lewiikamaa8@gmail.com>

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
 # Setup web server to serve static files
-if ! [ -x "$(command -v nginx)" ]; then
-    apt-get update && apt-get -y install nginx
-fi
+apt-get update
+apt-get -y install nginx
 mkdir -p /data/web_static/{releases/test,shared}
 echo "Testing 123" | sudo tee /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test /data/web_static/current
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 sed -i '/listen 80 default_server;/a location /hbnb_static/ {\n\talias /data/web_static/current/;\n}\n' /etc/nginx/sites-available/default
 service nginx restart
