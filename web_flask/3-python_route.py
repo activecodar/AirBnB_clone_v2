21e3a0be6becf665c042bbc2d1088f7631981eb7
activecodar <activecodar@gmail.com>
2023-04-21 07:41:47 +0300

    add a dynamic route with default value to a flask web app
    Signed-off-by: activecodar <activecodar@gmail.com>

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""
+This module defines the entrypoint for a
+simple Flask application.
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+    Returns a simple greeting message.
+
+    Returns:
+        str: A string containing message.
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Returns a simple message.
+
+    Returns:
+        str: A string message.
+    """
+    return 'HBNB'
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """
+    Returns a simple message.
+
+    Args:
+        text (str): The text to be displayed in the message.
+
+    Returns:
+        str: A string containing the
+        message with the specified text.
+    """
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """
+    Returns a simple message.
+
+    Args:
+        text (str): The text to be displayed in the message.
+
+    Returns:
+        str: A string containing the
+        message with the specified text.
+    """
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
